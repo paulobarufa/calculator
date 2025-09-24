811416946fc7e5a76eaa9ef3db9ca7c5eeaa2554
bigbarufa <paulo.matos16@gmail.com>
2025-09-24 15:02:33 +0100

    Fixed calculator functions
    Fixed calculator functions to display correct information on screen.

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ function divide(a, b) {
 
 function operate(a, b, operation) {
     switch (operation) {
-        case "+":
+        case "add":
             return add(a, b);
-        case "-":
+        case "subtract":
             return subtract(a, b);
-        case "/":
+        case "divide":
             return divide(a, b);
-        case "x":
+        case "multiply":
             return multiply(a, b);
         default:
             return 0;
@@ -38,5 +38,49 @@ let screen = document.getElementById("screen");
 let displayValue = 0;
 let storedValue = 0;
 let operator = "";
+
+let numArrays = document.querySelectorAll(".num-btn");
+numArrays.forEach(element => {
+    element.addEventListener("click", function(e){
+        let val = e.target.id.replace("k", "");
+        displayValue = displayValue == 0 ? val.toString() : displayValue + val.toString();
+        updateDisplay(displayValue);
+    });
+});
+
+let operatorArray = document.querySelectorAll(".op-btn");
+operatorArray.forEach(element => {
+    element.addEventListener("click", function(e){
+        let op = e.target.id.replace("k", "");
+
+        if (operator == "") {
+            
+            operator = op;
+            storedValue = displayValue;
+            displayValue = 0;
+
+        } else if (op == "equals") {
+
+            let newVal = operate(storedValue, displayValue, operator);
+
+            operator = "";
+            storedValue == displayValue;
+
+            displayValue = newVal.toString();
+            updateDisplay(displayValue);
+
+        } else {
+
+            let newVal = operate(storedValue, displayValue, op);
+            storedValue == displayValue;
+
+            displayValue = newVal.toString();
+            updateDisplay(displayValue);
+        }
+
+
+    });
+})
+
 
 
